2d0971b0ccd32401c114d9f38a6254372240a1b3
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-03-13 21:15:25 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import * as header from './js/header.js';
+import * as faq from './js/faq.js';
+import * as reviews from './js/reviews.js';
+import * as gallery from './js/gallery.js';
+import * as cookies from './js/cookies.js';
